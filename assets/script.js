c92be39d8fd5664b1bebc445f982a23a2d28a063
Eric Berman <ericb.dev48@gmail.com>
2024-10-07 12:40:54 -0400

    Added more JavaScript logic. Also slightly changed CSS.

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,32 @@
 const topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
-topics.forEach(e => {
-    console.log(e);    
-});
+const randomTopic = topics[Math.floor(Math.random() * topics.length)];
 
-/*
-if (topic === 'HTML') {
-    console.log("Let's study HTML!")
-} else if (topic === 'CSS') {
-    console.log("Let's study CSS!")
-} else if (topic === 'Git') {
-    console.log("Let's study Git!")
-} else if (topic === 'JavaScript') {
-    console.log("Let's study JavaScript!")
-} else {
-    console.log("Please try again!")
+let listTopics = () => {
+    console.log("Here are the topics we learned through Prework:")
+   topics.forEach(topic => {
+        console.log(topic);    
+    });
 }
-*/
+
+let selectTopic = (selectedTopic) => {
+    switch(selectedTopic) {
+        case "HTML":
+            console.log("Let's study HTML!");
+            break;
+        case "CSS":
+            console.log("Let's study CSS!");
+            break;    
+        case "Git":
+            console.log("Let's study Git!");
+            break;
+        case "JavaScript":
+            console.log("Let's study JavaScript!");
+            break;
+        default:
+            console.log("Error occurred.")
+    }
+}
+
+listTopics();
+console.log("Which topic should we study first?");
+selectTopic(randomTopic);
